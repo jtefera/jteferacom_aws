14912b402f90522d1d053ddd9e9a72e0e2f2ad23
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-16 14:38:13 -0800

    Changed 1out path to __dirname + path

--- src/server/server.js
+++ src/server/server.js
@@ -27,7 +27,7 @@ const PATHS = {
 app.use(morgan('combined'));
 let recipeServer = require("../projects/recipes_app_react/src/server/serverApp.js").app
 app.use("/recipes", recipeServer);
-app.use("/one_out", express.static('../src/projects/1out/public'));
+app.use("/one_out", express.static(__dirname + '/src/projects/1out/public/'));
 console.log(__dirname);
 //Static pages
 app.use(express.static('public'));
