5a5dd98c47851ccebebd2bf91cb494dd8ce932fa
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-17 19:25:05 -0800

    Changed back the webrootPath

--- src/server/server.js
+++ src/server/server.js
@@ -16,10 +16,8 @@ var lex = require('letsencrypt-express').create({
 
 // If you wish to replace the default plugins, you may do so here
 //
-, challenges: { 
-	'http-01': require('le-challenge-fs').create({ webrootPath: '~/letsencrypt/var/acme-challenges' })
-}
-, store: require('le-store-certbot').create({ webrootPath: '~/letsencrypt/var/acme-challenges' })
+, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
 
 // You probably wouldn't need to replace the default sni handler
 // See https://git.daplie.com/Daplie/le-sni-auto if you think you do
