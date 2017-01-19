e1c7c406f088c8776d224042596a68241aef40a0
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-18 17:52:22 -0800

    Changed fetch to jquery ajax

--- src/front/js/main.js
+++ src/front/js/main.js
@@ -1,33 +1,38 @@
 $('[data-toggle="tooltip"]').tooltip();
 
 $(document).ready(function() {
-  function loadIframeAndSilenceConsole(iframeId, url) {
-    $(iframeId).attr('src', url);
-    document.querySelector(iframeId).contentWindow.console.log = function() { /*nada}*/};
+  function loadIframeAndSilenceConsole(iframeSelector, url) {
+    $(iframeSelector).attr('src', url);
+    document.querySelector(iframeSelector).contentWindow.console.log = function() { /*nada}*/};
   }
-  console.log('Downloading iframes');
-  loadIframeAndSilenceConsole('#icmadrid-iframe', 'http://www.icmadrid.com');
+
+  function loadHigherResImgAndSub(imgSelector, imgUrl) {
+    $('<img />').attr('src', imgUrl).load(function() {
+      $(this).remove();
+      $(imgSelector).attr('src', imgUrl);
+    });
+  }
+
+  function loadHigherResBackgroundAndSub(elementSelector, imgUrl) {
+    $('<img />').attr('src', imgUrl).load(function() {
+      $(this).remove();
+      $(elementSelector).css('background-image', 'url(' + imgUrl + ')');
+    });
+  }
+  
+  
   loadIframeAndSilenceConsole('#recipes-iframe', '/recipes');
-  loadIframeAndSilenceConsole('#issues-iframe', 'http://icmadrid.com/issues');
-  loadIframeAndSilenceConsole('#podcasts-iframe', 'http://www.icmadrid.com/media');
   loadIframeAndSilenceConsole('#tictactoe-iframe', '//codepen.io/jonahum/embed/RagbgZ/?height=705&theme-id=0&default-tab=result&embed-version=2');
   loadIframeAndSilenceConsole('#pomodoro-iframe', '//codepen.io/jonahum/embed/WwjyjY/?height=700&theme-id=0&default-tab=result&embed-version=2');
   loadIframeAndSilenceConsole('#oneout-iframe', '/one_out');
-  $('<img />').attr('src', '../img/portfolio/spritesheet.png').load(function() {
-    console.log('downloaded high res image');
-    $(this).remove();
-    $('.portafolio-sprite').css('background-image', 'url(../img/portfolio/spritesheet.png)');
-  });
-  $('<img />').attr('src', '../img/programming_icons/spritesheet.png').load(function() {
-    console.log('downloaded high res image 2');
-    $(this).remove();
-    $('.frameworks-sprite').css('background-image', 'url(../img/programming_icons/spritesheet.png)');
-  });
-  $('<img />').attr('src', '../img/portfolio/cubli.gif').load(function() {
-    console.log('downloaded high res image 3');
-    $(this).remove();
-    $('#cubli-gif').attr('src', '../img/portfolio/cubli.gif');
-  });
+
+  loadHigherResBackgroundAndSub('.portafolio-sprite', '../img/portfolio/spritesheet.png');
+  loadHigherResBackgroundAndSub('.frameworks-sprite', '../img/programming_icons/spritesheet.png');
+
+  loadHigherResImgAndSub('#cubli-gif', '../img/portfolio/cubli.gif');
+  loadHigherResImgAndSub('#podcast-img', '../img/portfolio/icmadrid/podcast.png');
+  loadHigherResImgAndSub('#icmadrid-img', '../img/portfolio/icmadrid/icmadrid.png');
+  
 });
 
 
@@ -51,24 +56,22 @@ contactForm.addEventListener("submit", function (e) {
                 +"&message="+ encodeURIComponent(messageContact)
 
 
-  fetch("/send_contact", {
-    method: "POST",
+  $.ajax({
+    url: '/send_contact',
+    method: 'POST',
     headers: {
       "accept": "application/json",
       "cache-control": "no-cache",
       "content-type": "application/x-www-form-urlencoded"
     },
-    body: payload
-  })
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(data) {
-    $("#contactModalSuccess").modal('show')
-  })
-  .catch(function(e){
-    $("#contactModalError").modal('show')
+    data: payload,
+    error: function(response, status, errorText) {
+      $("#contactModalError").modal('show');
+      console.error(response, status, errorText);
+    },
+    success: function(response, status, xhr) {
+      $("#contactModalSuccess").modal('show');
+      console.log(response, status, xhr);
+    }
   });
-
-
-})
+});
